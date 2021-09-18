f4297e222b56b0ba3367734fdc3e882fb361008b
Verties <khalatyaaan@gmail.com>
2021-09-18 23:30:55 +0800

    loacl settings

---
+++ sibir/local_settings.py
@@ -0,0 +1,20 @@
+import pymysql
+pymysql.install_as_MySQLdb()
+# import os
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sibir',
+        'USER': 'u1455403_mysql',
+        'PASSWORD': '3cwQRTGbAS5Z2wF',
+        'HOST': '127.0.0.1',
+    }
+}
+
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'e3d9cd450949.ngrok.io']
